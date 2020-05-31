9dcf0097ea97b65678826136ddaac1ed2ad59378
Beyonce Ebo <manduulya@yahoo.com>
2020-05-31 18:51:19 -0400

    predeployment to heroku

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 const app = require("./app");
 const { PORT } = require("./config");
-var express = require("express");
 var path = require("path");
 var serveStatic = require("serve-static");
 
-var app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 
 app.listen(PORT, () => {
