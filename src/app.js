039aa968aea07107c4d990ec09f0e15dada5c52c
Beyonce Ebo <manduulya@yahoo.com>
2020-05-31 13:16:51 -0400

    Backend initial commitment

---
+++ src/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const app = express();
+const errorHandler = require("./error-handler");
+const dummyStore = require("./dummy-store");
+const morganOption = NODE_ENV === "production";
+const path = require("path");
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.get("/api/frames", (req, res) => {
+  res.send(dummyStore);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.use(errorHandler);
+module.exports = app;
---
+++ src/dummy-store.js
@@ -0,0 +1,46 @@
+module.exports = {
+  frames: [
+    {
+      id: 1,
+      name: "frame001",
+      urlName: "frame1",
+      dispImage: "/frameimages/frame001.png",
+      pricePerMeter: 1.2,
+    },
+    {
+      id: 2,
+      name: "frame002",
+      urlName: "frame2",
+      dispImage: "/frameimages/frame002.png",
+      pricePerMeter: 1.2,
+    },
+    {
+      id: 3,
+      name: "frame003",
+      urlName: "frame3",
+      dispImage: "/frameimages/frame003.png",
+      pricePerMeter: 1.2,
+    },
+    {
+      id: 4,
+      name: "frame004",
+      urlName: "frame4",
+      dispImage: "/frameimages/frame004.png",
+      pricePerMeter: 1.2,
+    },
+    {
+      id: 5,
+      name: "frame005",
+      urlName: "frame5",
+      dispImage: "/frameimages/frame005.png",
+      pricePerMeter: 1.2,
+    },
+    {
+      id: 6,
+      name: "frame006",
+      urlName: "frame6",
+      dispImage: "/frameimages/frame006.png",
+      pricePerMeter: 1.2,
+    },
+  ],
+};
